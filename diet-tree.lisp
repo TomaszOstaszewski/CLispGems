222ff8d457d2eeb1e59f3627ffa971dda16595f0
tomek <tomek@t60-0.lan>
2016-06-13 23:13:11 +0200

    Added DIET removal.

--- diet-tree.lisp
+++ diet-tree.lisp
@@ -105,11 +105,63 @@
                       :left (s-diet-node-left a-diet-node)
                       :right (s-diet-node-right a-diet-node)))
          (make-s-diet-node
-          :bottom (s-diet-node-right a-diet-node)
+          :bottom (s-diet-node-bottom a-diet-node)
           :top (s-diet-node-top a-diet-node)
           :left (s-diet-node-left a-diet-node)
           :right (diet-insert value (s-diet-node-right a-diet-node)))))
     (t a-diet-node)))
+
+(defun diet-merge (a-tree)
+  (if (zerop (random 2))
+      (multiple-value-bind (bottom top new-left-subtree)
+          (split-max (s-diet-node-left a-tree))
+        (make-s-diet-node :bottom bottom
+                          :top top
+                          :left new-left-subtree
+                          :right (s-diet-node-right a-tree)))
+      (multiple-value-bind (bottom top new-right-subtree)
+          (split-min (s-diet-node-right a-tree))
+        (make-s-diet-node :bottom bottom
+                          :top top
+                          :left (s-diet-node-left a-tree)
+                          :right new-right-subtree))))
+
+(defun diet-remove (a-val a-tree)
+  (let ((bottom (s-diet-node-bottom a-tree))
+        (top (s-diet-node-top a-tree))
+        (left (s-diet-node-left a-tree))
+        (right (s-diet-node-right a-tree)))
+    (cond
+      ;; Empty tree - trivial
+      ((null a-tree) nil)
+      ;; Lower than a bottom boundary
+      ;; advance to a left subtree
+      ((< a-val (s-diet-node-bottom a-tree))
+       (make-s-diet-node :bottom bottom :top top :left (diet-remove a-val left) :right right))
+      ;; Higher than a top boundary
+      ;; advance to a right subtree
+      ((> a-val (s-diet-node-top a-tree))
+       (make-s-diet-node :bottom bottom :top top :left left :right (diet-remove a-val right)))
+      ;; a-val belongs to the interval [bottom; top].
+      ;; Is it a single value interval?
+      ((eql bottom top) (diet-merge a-tree))
+      ;; Do we remove a bottom part?
+      ((eql a-val bottom) (make-s-diet-node :bottom (1+ bottom) :top top :left left :right right))
+      ;; Do we remova a top part?
+      ((eql a-val top) (make-s-diet-node :bottom bottom :top (1- top) :left left :right right))
+      ;; Neither a single value interval
+      ;; nor we shrank either bottom or top
+      ;; Need to split into 2 new nodes.
+      ;; We chose the split pattern in a random fashion
+      ;; 
+      (t
+       (if (zerop (random 2))
+           (make-s-diet-node :bottom bottom :top (1- a-val)
+                             :left left
+                             :right (make-s-diet-node :bottom (1+ a-val) :top top :left nil :right right))
+           (make-s-diet-node :bottom (1+ a-val) :top top
+                             :right right
+                             :left (make-s-diet-node :bottom bottom :top (1- a-val) :left left :right nil)))))))
 
 (defun all-but-leftmost-leaf (a-tree)
   (cond 

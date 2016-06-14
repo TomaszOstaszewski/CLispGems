cdbcb82e0792fa814b1fa7ef7048a23cb053ab1f
tomek <tomek@t60-0.lan>
2016-06-14 22:39:27 +0200

    Fixed diet-remove/merge.

--- diet-tree.lisp
+++ diet-tree.lisp
@@ -1,5 +1,7 @@
+(defstruct s-diet-node bottom top left right)
+
 (defun all-but-rightmost-leaf (a-tree)
-  (cond 
+  (cond
     ((null a-tree) nil)
     ((null (third a-tree)) (values (first a-tree) nil))
     (t (multiple-value-bind (node rest)
@@ -112,61 +114,87 @@
     (t a-diet-node)))
 
 (defun diet-merge (a-tree)
-  (if (zerop (random 2))
-      (multiple-value-bind (bottom top new-left-subtree)
-          (split-max (s-diet-node-left a-tree))
-        (make-s-diet-node :bottom bottom
-                          :top top
-                          :left new-left-subtree
-                          :right (s-diet-node-right a-tree)))
-      (multiple-value-bind (bottom top new-right-subtree)
-          (split-min (s-diet-node-right a-tree))
-        (make-s-diet-node :bottom bottom
-                          :top top
-                          :left (s-diet-node-left a-tree)
-                          :right new-right-subtree))))
+  (cond
+    ((null (s-diet-node-right a-tree)) (s-diet-node-left a-tree))
+    ((null (s-diet-node-left a-tree)) (s-diet-node-right a-tree))
+    (t
+     (if (zerop (random 2))
+         (multiple-value-bind (bottom top new-left-subtree)
+             (split-max (s-diet-node-left a-tree))
+           (make-s-diet-node :bottom bottom :top top
+                             :left new-left-subtree :right (s-diet-node-right a-tree)))
+         (multiple-value-bind (bottom top new-right-subtree)
+             (split-min (s-diet-node-right a-tree))
+           (make-s-diet-node :bottom bottom :top top
+                             :left (s-diet-node-left a-tree) :right new-right-subtree))))))
 
 (defun diet-remove (a-val a-tree)
-  (let ((bottom (s-diet-node-bottom a-tree))
-        (top (s-diet-node-top a-tree))
-        (left (s-diet-node-left a-tree))
-        (right (s-diet-node-right a-tree)))
-    (cond
-      ;; Empty tree - trivial
-      ((null a-tree) nil)
-      ;; Lower than a bottom boundary
-      ;; advance to a left subtree
-      ((< a-val (s-diet-node-bottom a-tree))
-       (make-s-diet-node :bottom bottom :top top :left (diet-remove a-val left) :right right))
-      ;; Higher than a top boundary
-      ;; advance to a right subtree
-      ((> a-val (s-diet-node-top a-tree))
-       (make-s-diet-node :bottom bottom :top top :left left :right (diet-remove a-val right)))
-      ;; a-val belongs to the interval [bottom; top].
-      ;; Is it a single value interval?
-      ((eql bottom top) (diet-merge a-tree))
-      ;; Do we remove a bottom part?
-      ((eql a-val bottom) (make-s-diet-node :bottom (1+ bottom) :top top :left left :right right))
-      ;; Do we remova a top part?
-      ((eql a-val top) (make-s-diet-node :bottom bottom :top (1- top) :left left :right right))
-      ;; Neither a single value interval
-      ;; nor we shrank either bottom or top
-      ;; Need to split into 2 new nodes.
-      ;; We chose the split pattern in a random fashion
-      ;; 
-      (t
-       (if (zerop (random 2))
-           (make-s-diet-node :bottom bottom :top (1- a-val)
-                             :left left
-                             :right (make-s-diet-node :bottom (1+ a-val) :top top :left nil :right right))
-           (make-s-diet-node :bottom (1+ a-val) :top top
-                             :right right
-                             :left (make-s-diet-node :bottom bottom :top (1- a-val) :left left :right nil)))))))
-
-(defun all-but-leftmost-leaf (a-tree)
-  (cond 
+  (cond
+    ;; Empty tree - trivial
     ((null a-tree) nil)
-    ((null (second a-tree)) (values (first a-tree) nil))
-    (t (multiple-value-bind (node rest)
-           (all-but-leftmost-leaf (second a-tree))
-         (values node (list (first a-tree) rest (third a-tree)))))))
+    (t
+     (let ((bottom (s-diet-node-bottom a-tree))
+           (top (s-diet-node-top a-tree))
+           (left (s-diet-node-left a-tree))
+           (right (s-diet-node-right a-tree)))
+       (assert (and (not (null bottom))
+                    (not (null top))))
+       (cond
+         ;; Lower than a bottom boundary
+         ;; advance to a left subtree
+         ((< a-val bottom)
+          (make-s-diet-node :bottom bottom :top top :left (diet-remove a-val left) :right right))
+          ;; Higher than a top boundary
+         ;; advance to a right subtree
+         ((> a-val top)
+          (make-s-diet-node :bottom bottom :top top :left left :right (diet-remove a-val right)))
+         ;; a-val belongs to the interval [bottom; top].
+         ;; Is it a single value interval?
+         ((eql bottom top) (diet-merge a-tree))
+         ;; Do we remove a bottom part?
+         ((eql a-val bottom) (make-s-diet-node :bottom (1+ bottom) :top top :left left :right right))
+         ;; Do we remova a top part?
+         ((eql a-val top) (make-s-diet-node :bottom bottom :top (1- top) :left left :right right))
+         ;; Neither a single value interval
+         ;; nor we shrank either bottom or top
+         ;; Need to split into 2 new nodes.
+         ;; We chose the split pattern in a random fashion
+         ;;
+         (t
+          (if (zerop (random 2))
+              (make-s-diet-node :bottom bottom :top (1- a-val) :left left
+                                :right (make-s-diet-node :bottom (1+ a-val) :top top :left nil :right right))
+              (make-s-diet-node :bottom (1+ a-val) :top top :right right
+                                :left (make-s-diet-node :bottom bottom :top (1- a-val) :left left :right nil)))))))))
+
+(defun diet-height (a-tree)
+  (if (null a-tree)
+      0
+      (+ 1
+         (diet-height (s-diet-node-right a-tree))
+         (diet-height (s-diet-node-left a-tree)))))
+
+(defun diet-balance (a-tree)
+  (cond
+    ((null a-tree) 0)
+    ((and (null (s-diet-node-left a-tree)) (null (s-diet-node-right a-tree))) 0)
+    (t (- (diet-height (s-diet-node-right a-tree))
+          (diet-height (s-diet-node-left a-tree))))))
+
+
+(defun diet-max-interval (a-tree)
+  (cond
+    ((null a-tree) 0)
+    ((and (null (s-diet-node-left a-tree)) (null (s-diet-node-right a-tree)))
+         (1+ (- (s-diet-node-top a-tree) (s-diet-node-bottom a-tree))))
+    ((null (s-diet-node-left a-tree))
+     (max (1+ (- (s-diet-node-top a-tree) (s-diet-node-bottom a-tree)))
+          (diet-max-interval (s-diet-node-right a-tree))))
+    ((null (s-diet-node-right a-tree))
+     (max (1+ (- (s-diet-node-top a-tree) (s-diet-node-bottom a-tree)))
+          (diet-max-interval (s-diet-node-left a-tree))))
+    (t
+     (max (diet-max-interval (s-diet-node-left a-tree))
+          (diet-max-interval (s-diet-node-right a-tree))))))
+
+

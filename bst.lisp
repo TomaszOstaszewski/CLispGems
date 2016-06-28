3a64127bc8a737b56d9d30b7f469e4a55c4e2fbe
Tomasz Ostaszewski <tomasz.ostaszewski@interia.pl>
2016-06-29 00:32:20 +0200

    Added traverse functions.

--- bst.lisp
+++ bst.lisp
@@ -18,7 +18,10 @@
           ((> a-val val) (bst-member? a-val right))
           (t t)))))
 
-
+;; Walks a tree constantly to the right and calls a collector function
+;; with two paramters:
+;; - a value of the maximum node - a node with no right child
+;; - a tree without this maximum node
 (defun split-max (a-tree collector)
   (if (null a-tree)
       (funcall collector nil nil)
@@ -29,6 +32,10 @@
                        #'(lambda(last-val new-tree)
                            (funcall collector last-val (make-bst-node :val val :left left :right new-tree))))))))
 
+;; Walks a tree constantly to the left and calls a collector function
+;; with two paramters:
+;; - a value of the minimum node - a node with no left child
+;; - a tree without this minimum node
 (defun split-min (a-tree collector)
   (if (null a-tree)
       (funcall collector nil nil)
@@ -39,6 +46,11 @@
                        #'(lambda(last-val new-tree)
                            (funcall collector last-val (make-bst-node :val val :left new-tree :right right))))))))
 
+;; Knuth's delete by copying
+;; When a node is to be deleted, we find its successor/predecessor
+;; Then we create a new node, with this successor's value
+;; and left/rigth subtree set to the successor's left/right subtree
+;; and right/left subtree set to this node's right/left subtree.
 (defun bst-delete (a-val a-root)
   (if (null a-root)
       nil
@@ -47,9 +59,33 @@
           ((< a-val val) (make-bst-node :val val :left (bst-delete a-val left) :right right))
           ((> a-val val) (make-bst-node :val val :left left :right (bst-delete a-val right)))
           (t (if (zerop (random 2))
-                 (split-max (bst-node-left a-root)
+                 ; Replace a node with its predecessor
+                 (split-max left
                             #'(lambda (new-root-val max-left-subtree)
                                 (make-bst-node :val new-root-val :left max-left-subtree :right right)))
-                 (split-min (bst-node-right a-root)
+                 ; Replace a node with its successor
+                 (split-min right
                             #'(lambda (new-root-val min-right-subtree)
                                 (make-bst-node :val new-root-val :left left :right min-right-subtree)))))))))
+
+(defun bst-preorder (a-root fn)
+  (when a-root
+    (with-slots (val left right) a-root
+      (funcall fn val)
+      (bst-preorder left fn)
+      (bst-preorder right fn))))
+
+(defun bst-inorder (a-root fn)
+  (when a-root
+    (with-slots (val left right) a-root
+      (bst-preorder left fn)
+      (funcall fn val)
+      (bst-preorder right fn))))
+
+
+(defun bst-postorder (a-root fn)
+  (when a-root
+    (with-slots (val left right) a-root
+      (bst-preorder left fn)
+      (bst-preorder right fn)
+      (funcall fn val))))
